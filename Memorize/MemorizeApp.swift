a497e1bfb260f8dc7ac23b154ac9c4c207ab4a3a
fei.chen <fchen24@outlook.com>
2021-05-29 14:11:01 +0800

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by 陳飛 on 29/5/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
